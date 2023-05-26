126ce0e17019f064ec763bd8b0a37e132b3d9cc8
fede-marra <fedemarra@gmail.com>
2023-05-26 19:33:29 -0300

    create and retrieve client

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 	"time"
 )
 
 type client struct {
-	name    string
-	address string
-	phone   string
-	email   string
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Phone   string `json:"phone"`
+	Email   string `json:"email"`
 }
 
 type credentials struct {
@@ -33,7 +35,65 @@ type jobs struct {
 	clientName string
 }
 
+func handleClient(w http.ResponseWriter, req *http.Request) {
+
+	switch req.Method {
+	case "GET":
+		id := req.URL.Query().Get("id")
+		if id == "" {
+			clients := []client{}
+
+			jsonResp, err := json.Marshal(clients)
+			if err != nil {
+				log.Printf("Error happened in JSON marshal. Err: %s\n", err)
+			}
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonResp)
+		} else {
+			client := client{}
+			jsonResp, err := json.Marshal(client)
+			if err != nil {
+				log.Printf("Error happened in JSON marshal. Err: %s\n", err)
+			}
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonResp)
+		}
+
+		return
+
+	case "POST":
+		var client client
+		err := json.NewDecoder(req.Body).Decode(&client)
+
+		if err != nil {
+			log.Printf("Error happened in JSON marshal. Err: %s\n", err)
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte{})
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+		jsonResp, err := json.Marshal(client)
+		if err != nil {
+			log.Printf("Error happened in JSON marshal. Err: %s\n", err)
+		}
+		w.Write(jsonResp)
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte{})
+		return
+	}
+}
+
 func main() {
-	fmt.Println(client{}, resource{}, jobs{})
-	
+
+	http.HandleFunc("/client", handleClient)
+
+	http.ListenAndServe(":8080", nil)
 }
